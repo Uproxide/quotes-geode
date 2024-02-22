9b6078053d142bd70128f0367310cd40de4ebd96
Coop <73203995+coopeeo@users.noreply.github.com>
2024-02-22 16:48:23 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include <Geode/ui/GeodeUI.hpp>
 #include <Geode/loader/Loader.hpp>
 #include <Geode/loader/Mod.hpp>
+// Thanks so much Jouca and Firee
+#include "CustomSettings.hpp"
 
 using namespace geode::prelude;
 
@@ -17,101 +19,29 @@ void refreshFunc() {
     .fetch("http://projectbdash.com/api/v1/funfacts/fact/1.0.1.php")
     .json()
     .then([this](auto const& json) {
-		matjson::Value json = this
+		//matjson::Value json = this
 		Mod::get()->setSavedValue<std::string>("list", json.dump(matjson::NO_INDENTATION));
-    });
-    //.expect((std::string error) {});
+    })
+    .expect((std::string error) {});
 }
-
-class $modify(refresh,MenuLayer) {
-	void onRefreshPress(CCObject*) {
-		refreshFunc();
-	}
-};
 
 
 // Stole from gd utils because stuff on the docs wouldent work so yea
-class SettingRefreshValue;
-
-class SettingRefreshValue : public SettingValue {
-protected:
-    std::string m_placeholder;
-public:
-    SettingRefreshValue(std::string const& key, std::string const& modID, std::string const& placeholder)
-      : SettingValue(key, modID), m_placeholder(placeholder) {}
-
-    bool load(matjson::Value const& json) override {
-        return true;
-    }
-    bool save(matjson::Value& json) const override {
-        return true;
-    }
-    SettingNode* createNode(float width) {
-    	return SettingRefreshNode::create(this, width);
-    }
-};
-
-
-class SettingRefreshNode : public SettingNode {
-protected:
- bool init(SettingRefreshValue* value, float width) {
-        if (!SettingNode::init(value))
-            return false;
-        this->setContentSize({ width, 35.f });
-        auto menu = CCMenu::create();
-        auto label = CCLabelBMFont::create("Refresh Quotes", "bigFont.fnt");
-        label->setScale(.6F);
-        label->setPositionX(-130);
-        auto spr = CCSprite::createWithSpriteFrameName("GJ_updateBtn_001.png");
-        spr->setScale(.4F);
-        auto refreshBtn = CCMenuItemSpriteExtra::create(
-            playSpr,
-            this,
-            menu_selector(SettingRefreshNode::onRefreshBtn)
-        );
-        refreshBtn->setPositionX(140);
-        menu->setPosition(width / 2, 18.f);
-        menu->addChild(label);
-        menu->addChild(refreshBtn);
-        this->addChild(menu);
-        return true;
-    }
-
-public:
-    void onRefreshBtn(CCObject*) {
-	    refreshFunc();
-    }
-
-    void commit() override {
-        this->dispatchCommitted();
-    }
-
-    bool hasUncommittedChanges() override {
-        return false;
-    }
-
-
-    bool hasNonDefaultValue() override {
-        return true;
-    }
-	    
-    void resetToDefault() override {
-
-    }
-    static SettingRefreshNode* create(SettingRefreshValue* value, float width) {
-        auto ret = new SettingRefreshNode;
-        if (ret && ret->init(value, width)) {
-            ret->autorelease();
-            return ret;
-        }
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
-};
-// end of stealing from GDUtils THANKS!
-$on_mod(Loaded) {
-    Mod::get()->addCustomSetting<MySettingValue>("refresh-btn");
+SettingNode* SettingRefreshValue::createNode(float width) {
+    return SettingRefreshNode::create(this, width);
 }
+
+void SettingRefreshNode:onRefreshBtn(CCObject*) {
+	    refreshFunc();
+}
+
+
+
+$on_mod(Loaded) {
+    Mod::get()->addCustomSetting<SettingTestValue>("refresh-btn", "none");
+}
+// end of stealing from GDUtils THANKS!
+
 
 class $modify(funFacts, MenuLayer) {
 	std::string funFact;
