10df14e970174519783d5628b948832ae0cf582b
Coop <73203995+coopeeo@users.noreply.github.com>
2024-02-22 18:49:59 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ class $modify(funFacts, MenuLayer) {
 	void onFunFact(CCObject*) {
 		auto data = Mod::get()->getSavedValue<std::string>("list");
 		auto json2 = matjson::parse(fmt::format("{{\"thereeldata\":{} }}", data));
-		auto json2 = json2["thereeldata"]
+		auto json = json2["thereeldata"]
 		//auto resAsVector = json.as<std::array>();
 				auto& firstObject = json[rand() % json.size()];
 				std::string quote = fmt::format("{}\nBy {}", firstObject["funFact"], firstObject["userOfReq"]);
